9acc4f454fd68e460831b2796d39b22908f4b7c5
Zak <zakariabouaddou@gmail.com>
2024-10-22 19:53:21 -0700

    Adjusting header code to include description of functionality

--- Header.h
+++ Header.h
@@ -8,11 +8,11 @@ string input;
 void ValidateCustomerNumber(const string& input) {
     if (input.length() == 6 && isalpha(input[0]) && isalpha(input[1]) &&
         isdigit(input[2]) && isdigit(input[3]) &&
-        isdigit(input[4]) && isdigit(input[5])) {
-        cout << "Valid customer ID " << endl;
+        isdigit(input[4]) && isdigit(input[5])) { // make condition statement to first check if input length is 6 , then if first indexes 0 and 1 are alphabetical, and finally taking substring for last 4 characterers to check if they are digits
+        cout << "Valid customer ID " << endl; // indicate validity message
     }
     else
-        cout << "Invalid customer ID " << endl;
+        cout << "Invalid customer ID " << endl; // otherwise not validate
 
 }
 
